9d796aa3c9c185b4ae2fcce52498ee1ac5622b76
ACooperHMCTS <ashley.cooper@hmcts.net>
2026-07-22 16:01:04 +0100

    Adjusted the folders for R1A and R1B

--- src/gatling/java/simulations/Scripts/ScenarioBuilder/R1B/R1bMajorCreditorSearchBuild.java
+++ src/gatling/java/simulations/Scripts/ScenarioBuilder/R1B/R1bMajorCreditorSearchBuild.java
@@ -1,4 +1,4 @@
-package simulations.Scripts.ScenarioBuilder;
+package simulations.Scripts.ScenarioBuilder.R1B;
 
 import io.gatling.javaapi.core.ChainBuilder;
 import io.gatling.javaapi.core.ScenarioBuilder;
