06fb04399d3f917ca6d9e50e950a1d6e1cf60883
ACooperHMCTS <ashley.cooper@hmcts.net>
2026-08-20 16:18:17 +0100

    Added R1B simulation

--- src/gatling/java/simulations/Scripts/Utilities/SimulationNames.java
+++ src/gatling/java/simulations/Scripts/Utilities/SimulationNames.java
@@ -4,4 +4,6 @@ public class SimulationNames {
 
     public static final String OPAL_LOGIN_TEST = "Opal Manual Account Creation Test";
     public static final String MAC_01A_TEST    = "MAC 01a Test";
+    public static final String MAC_01B_TEST    = "MAC 01b Test";
+
 }
